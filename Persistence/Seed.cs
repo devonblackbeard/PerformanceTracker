11605fd2e2734e6b3696572c848b21e11376a5cf
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2022-02-10 20:25:33 -0700

    Create Data context

---
+++ Persistence/Seed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Workouts.Any()) return;
+
+            var workouts = new List<Workout>
+            {
+                new Workout
+                {
+                    Name = "Chest and Back",                    
+                },
+                new Workout
+                {
+                    Name = "Legs and Back"                   
+                },
+
+            };
+
+            await context.Workouts.AddRangeAsync(workouts);
+            await context.SaveChangesAsync();
+        }
+    }
+}
